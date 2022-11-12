a93fd23a0de65d6ab3ab235ca0bec3ab5ae313ab
Кариша <lucaslima2458@gmail.com>
2022-11-12 09:54:53 -0300

    chore: add answer to the exercise

--- 03/index.js
+++ 03/index.js
@@ -1,2 +1,26 @@
 const numeros = [54, 22, 14, 87, 10, 284];
 
+for (const num of numeros) {
+    if (num === 10) console.log(numeros.indexOf(num));
+    else if (!numeros.includes(10)) { 
+        console.log(-1); 
+        break;
+    }
+}
+
+
+// let count = 0;
+
+// for (const num of numeros) {
+//     if (num === 10) {
+//         console.log(numeros.indexOf(num))
+//         break;
+//     }
+
+//     else { 
+//         count++;
+//         console.log(-1);
+//         break;
+//     }
+// }
+
