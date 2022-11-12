4d3160a8b3694872f8d6bbe8c5115693efae8b5b
Кариша <lucaslima2458@gmail.com>
2022-11-12 10:29:39 -0300

    chore: add correct array elements

--- 09/index.js
+++ 09/index.js
@@ -1,2 +1,6 @@
 
 const filaDePedidos = ["pedido 12", "pedido 13", "pedido 14"];
+
+const resposta = filaDePedidos.push("pedido 15") && filaDePedidos.shift("pedido12");
+
+console.log(filaDePedidos);
