701548029c13d95fb839a3cb3fd9c2757611b5cf
Кариша <lucaslima2458@gmail.com>
2022-11-12 17:22:20 -0300

    chore: add answer to the question

--- 15/index.js
+++ 15/index.js
@@ -2,3 +2,8 @@ const original = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 const pares = [];
 const impares = [];
 
+original.filter(num => num % 2 === 0 && pares.push(num));
+original.filter(num => num % 2 !== 0 && impares.push(num));
+
+console.log(pares)
+console.log(impares)
