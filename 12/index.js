3c6328e39e4376bd18fcb8ce1f34ce152a19c8e2
Кариша <lucaslima2458@gmail.com>
2022-11-13 16:03:15 -0300

    chore: add answer to the question

--- 12/index.js
+++ 12/index.js
@@ -1,3 +1,13 @@
 const filaDeDentro = ["Jose", "Maria", "Joao"];
 const filaDeFora = ["Joana", "Roberta", "Marcos", "Felipe"];
 
+
+for (const name of filaDeFora) {
+    let shiftedPerson = filaDeFora.shift()
+    if (filaDeDentro.length < 5 ) filaDeDentro.push(shiftedPerson)
+
+    else { break; }
+}
+
+console.log(filaDeDentro)
+console.log(filaDeFora);
