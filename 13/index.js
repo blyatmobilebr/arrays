ab08b5cb7bf35153667799a9e1f650efa30478a8
Кариша <lucaslima2458@gmail.com>
2022-11-13 15:55:12 -0300

    chore: add answer to the question

--- 13/index.js
+++ 13/index.js
@@ -1,2 +1,6 @@
 const disjuntores = [false, false, true, false, false, true, false, false];
 
+
+// Array.prototype.forEach() applies a callback function on each element 
+// and returns an array of the elements that satisfy a certain condition
+disjuntores.forEach((item, index) => item === true ? console.log(index) : -1)
